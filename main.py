61c4ea751b23acf543dc02793abd8aaab58ab88a
tim-zh <tim.zhlobich@gmail.com>
2019-08-03 15:33:38 +0300

    pygame window

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import pygame
+from pygame.locals import *
+
+SCREENRECT = Rect(0, 0, 640, 480)
+
 def main():
-    print("hello, world")
+    pygame.init()
+    winstyle = 0  # FULLSCREEN
+    bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
+    screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
+    pygame.display.set_caption('py sweeper')
+
+    pygame.time.wait(1000)
+    pygame.quit()
 
 
 if __name__ == '__main__':
