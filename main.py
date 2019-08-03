e0d4d54edd4360609771100d319fd775ce397d3f
tim-zh <tim.zhlobich@gmail.com>
2019-08-03 15:41:30 +0300

    background image

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import os
 import pygame
 from pygame.locals import *
 
 SCREENRECT = Rect(0, 0, 640, 480)
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+
+def load_image(file):
+    """loads an image, prepares it for play"""
+    file = os.path.join(main_dir, 'data', file)
+    try:
+        surface = pygame.image.load(file)
+    except pygame.error:
+        raise SystemExit('Could not load image "%s" %s' % (file, pygame.get_error()))
+    return surface.convert()
+
 
 def main():
     pygame.init()
@@ -9,6 +23,14 @@ def main():
     bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
     pygame.display.set_caption('py sweeper')
+
+    bgdtile = load_image('sprite.png')
+    background = pygame.Surface(SCREENRECT.size)
+    for x in range(0, SCREENRECT.width, bgdtile.get_width()):
+        for y in range(0, SCREENRECT.height, bgdtile.get_height()):
+            background.blit(bgdtile, (x, y))
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
 
     pygame.time.wait(1000)
     pygame.quit()
