6c7f02819e2f34bc9bb55cee163beccb14d53617
tim-zh <tim.zhlobich@gmail.com>
2019-08-13 17:01:18 +0300

    refactor in more pygame way, mouse events

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import math
 import pygame
 from pygame.locals import *
 
@@ -28,34 +29,38 @@ def load_tileset(file, num_x, num_y):
     return result
 
 
-class Cell:
-    type = 0
-    open = False
+class Cell(pygame.sprite.Sprite):
+    images = []
+    tile_width = 0
+    tile_height = 0
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self._is_open = False
+        self.type = 12
+        self.image = self.images[self.type]
+        self.rect = self.image.get_rect()
+        self.rect.left = x * Cell.tile_width
+        self.rect.top = y * Cell.tile_height
+
+    def open(self):
+        self._is_open = True
+        self.type = 0
+
+    def update(self):
+        self.image = self.images[self.type]
 
 
 class MineField:
-    def __init__(self, tile_set, num_x, num_y):
-        self.tile_set = tile_set
-        self.tile_width = tile_set[0].get_width()
-        self.tile_height = tile_set[0].get_height()
+    def __init__(self, num_x, num_y):
         self.num_x = num_x
         self.num_y = num_y
-        self.surface = pygame.Surface((self.tile_width * num_x, self.tile_height * num_y))
         self.cells = []
-        for x in range(0, num_x + 2):
+        for x in range(0, num_x):
             row = []
-            for y in range(0, num_y + 2):
-                row.append(Cell())
+            for y in range(0, num_y):
+                row.append(Cell(x, y))
             self.cells.append(row)
-
-    def render(self, screen):
-        for x in range(1, self.num_x + 1):
-            for y in range(1, self.num_y + 1):
-                if self.cells[x][y].open:
-                    self.surface.blit(self.tile_set[0], (self.tile_width * (x - 1), self.tile_height * (y - 1)))
-                elif self.cells[x][y].type == 0:
-                    self.surface.blit(self.tile_set[12], (self.tile_width * (x - 1), self.tile_height * (y - 1)))
-        screen.blit(self.surface, (0, 0))
 
 
 def main():
@@ -63,18 +68,36 @@ def main():
     winstyle = 0  # FULLSCREEN
     screen_rect = (10, 10)
     bestdepth = pygame.display.mode_ok(screen_rect, winstyle, 32)
-    screen = pygame.display.set_mode(screen_rect, winstyle, bestdepth)
+    pygame.display.set_mode(screen_rect, winstyle, bestdepth)
     pygame.display.set_caption('py sweeper')
 
-    cells = load_tileset('cells.png', 9, 2)
-    field = MineField(cells, 9, 9)
-    screen_rect = (field.num_x * field.tile_width, field.num_y * field.tile_height)
-    screen = pygame.display.set_mode(screen_rect, winstyle, bestdepth)
-    field.render(screen)
-    pygame.display.flip()
+    cells_group = pygame.sprite.RenderUpdates()
+    Cell.containers = cells_group
 
-    pygame.time.wait(1000)
-    pygame.quit()
+    Cell.images = load_tileset('cells.png', 9, 2)
+    Cell.tile_width = Cell.images[0].get_width()
+    Cell.tile_height = Cell.images[0].get_height()
+    field = MineField(9, 9)
+    screen_rect = (field.num_x * Cell.tile_width, field.num_y * Cell.tile_height)
+    screen = pygame.display.set_mode(screen_rect, winstyle, bestdepth)
+
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                return
+            if event.type == MOUSEBUTTONUP:
+                pos_x, pos_y = pygame.mouse.get_pos()
+                x = math.floor(pos_x / Cell.tile_width)
+                y = math.floor(pos_y / Cell.tile_height)
+                field.cells[x][y].open()
+
+        cells_group.update()
+        dirty = cells_group.draw(screen)
+        pygame.display.update(dirty)
+
+        clock.tick(40)
 
 
 if __name__ == '__main__':
